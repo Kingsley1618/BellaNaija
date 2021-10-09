affdd625b9a45da6e890d2d397cff5359142c6e3
Kingsley1618 <89419401+Kingsley1618@users.noreply.github.com>
2021-10-08 18:33:48 -0700

    Add files via upload

---
+++ BellanaijaJavascript.js
@@ -0,0 +1,528 @@
+var a = document.getElementById("headFirst");
+a.style.borderBottom = "2px solid red";
+
+
+
+function myFunc() {
+	document.getElementById("headSecond").onclick = function() {
+
+
+
+
+		if (a.style.borderBottom == "2px solid red") {
+			a.style.borderBottom = "none";
+
+			document.getElementById("headSecond").style.borderBottom = "2px solid red";
+
+			document.getElementById("secondImg").src ="thumbnail_1280X720-2021-10-05T111918.343-80x80.jpg";
+			document.getElementById("secondText").innerHTML = "Make Thirst-Trapping Chicken Stew with Kikifoodies'Simple Recipe";
+
+			document.getElementById("thirdText").innerHTML = "Femi Adebayo Premieres Episode 5 of Comedy Series 'Sisi' Season 3";
+			document.getElementById("thirdImg").src ="thumbnail_1280X720-2021-10-05T110152.723-400x240.jpg";
+			document.getElementById("thirdImg").style.width = "80px";
+
+			document.getElementById("fourthText").innerHTML = "Zeelicious'Budget-Friendly Meal Ideas-Yam & Palm Oil Sauce + Moi-Moi Recipes";
+			document.getElementById("fourthImg").src ="thumbnail_1280X720-2021-10-05T102411.023-80x80.jpg";
+
+			document.getElementById("fifthText").innerHTML = "Must Watch Trailer:Bimbo Mauel,Zainab Balogun,Stan Eze in Full-Option Adventure'Charge & Bail'";
+			document.getElementById("fifthImg").src ="IMG-3672-80x80.jpg";
+
+			document.getElementById("sixthText").innerHTML = "The First Episode ofDodos Uvhierghara's Talk Show 'To all The Girls'Centers on Marraige | WATCH";
+			document.getElementById("sixthImg").src ="thumbnail_1280X720-2021-10-04T181134.694-400x240.jpg";
+			document.getElementById("sixthImg").style.width = "80px";
+
+
+
+			document.getElementById("seventhText").innerHTML = "Here's Episode 10 of Besketmouth's Comedy Series 'Papa Benji'Season 2";
+			document.getElementById("seventhImg").src ="thumbnail_1280X720-2021-10-04T144146.752-400x240.jpg";
+			document.getElementById("seventhImg").style.width = "80px";
+
+			document.getElementById("eightText").innerHTML = "LyricVideo:Sinach-Love My Heine";
+			document.getElementById("eightImg").src ="thumbnail_1280X720-2021-10-04T143429.729-400x240.jpg";
+			
+
+
+	}
+}
+}
+
+myFunc();
+
+
+
+function myFun() {
+	document.getElementById("headFirst").onclick = function() {
+		if (document.getElementById("headSecond").style.borderBottom == "2px solid red") {
+			document.getElementById("headSecond").style.borderBottom = "none";
+			a.style.borderBottom = "2px solid red";
+
+			document.getElementById("secondImg").src ="Skaahphotography-1323-80x80.jpg";
+			document.getElementById("secondText").innerHTML = "Fredom Foundation lauches 'Operation Better Life' to curb Drug Abuse in Lagos State";
+
+			 document.getElementById("thirdImg").src ="IMG-3672-80x80.jpg";
+
+			document.getElementById("thirdText").innerHTML = "Must Watch Trailer:Bimbo Mauel,Zainab Balogun,Stan Eze in Full-Option Adventure'Charge & Bail'";
+
+			document.getElementById("fourthText").innerHTML = "Your Better Self with Akanna:5 Life Lessons You Can Clean From a Colossal Failure";
+			document.getElementById("fourthImg").src ="Akanna-Okeke-YourBetterSelf-80x80.png";
+
+			
+			document.getElementById("fifthText").innerHTML = "New Music:Shime Ahua-Smile";
+			document.getElementById("fifthImg").src ="Shime-Ahua-80x80.jpg";
+
+
+			document.getElementById("sixthText").innerHTML = "Zeelicious'Budget-Friendly Meal Ideas-Yam & Palm Oil Sauce + Moi-Moi Recipes";
+			document.getElementById("sixthImg").src ="thumbnail_1280X720-2021-10-05T102411.023-80x80.jpg";
+
+			document.getElementById("seventhText").innerHTML = "Make Thirst-Trapping Chicken Stew with Kikifoodies'Simple Recipe";
+			document.getElementById("seventhImg").src ="thumbnail_1280X720-2021-10-05T111918.343-80x80.jpg";
+
+			document.getElementById("eightText").innerHTML = "Femi Adebayo Premieres Episode 5 of Comedy Series 'Sisi' Season 3";
+			document.getElementById("eightImg").src ="thumbnail_1280X720-2021-10-05T110152.723-400x240.jpg";
+
+
+
+
+		} 
+
+
+	}
+}
+
+myFun();
+
+function myFu() {
+	document.getElementById("headSecond").onmouseover = function() {
+		document.getElementById("headSecond").style.cursor = "pointer";
+	}
+}
+
+myFu();
+
+
+
+function myF() {
+	document.getElementById("headFirst").onmouseover = function() {
+		document.getElementById("headFirst").style.cursor = "pointer";
+	}
+}
+
+myF();
+
+
+function hoverFirst() {
+	document.getElementById("first").onmouseenter = function () {
+		document.getElementById("first").style.color = "orange";
+	}
+}
+
+hoverFirst();
+
+
+function hoverSecond() {
+	document.getElementById("first").onmouseleave = function () {
+		document.getElementById("first").style.color = "black";
+	}
+}
+
+hoverSecond();
+
+function hoverFirs() {
+	document.getElementById("second").onmouseenter = function () {
+		document.getElementById("second").style.color = "orange";
+	}
+}
+
+hoverFirs();
+
+
+function hoverSecon() {
+	document.getElementById("second").onmouseleave = function () {
+		document.getElementById("second").style.color = "black";
+	}
+}
+
+hoverSecon();
+
+
+function hoverFir() {
+	document.getElementById("third").onmouseenter = function () {
+		document.getElementById("third").style.color = "orange";
+	}
+}
+
+hoverFir();
+
+
+function hoverSeco() {
+	document.getElementById("third").onmouseleave = function () {
+		document.getElementById("third").style.color = "black";
+	}
+}
+
+hoverSeco();
+
+
+
+
+
+function hoverFi() {
+	document.getElementById("fourth").onmouseenter = function () {
+		document.getElementById("fourth").style.color = "orange";
+	}
+}
+
+hoverFi();
+
+
+function hoverSec() {
+	document.getElementById("fourth").onmouseleave = function () {
+		document.getElementById("fourth").style.color = "black";
+	}
+}
+
+hoverSec();
+
+function hoverF() {
+	document.getElementById("fifth").onmouseenter = function () {
+		document.getElementById("fifth").style.color = "orange";
+	}
+}
+
+hoverF();
+
+
+function hoverSe() {
+	document.getElementById("fifth").onmouseleave = function () {
+		document.getElementById("fifth").style.color = "black";
+	}
+}
+
+hoverSe();
+
+
+function sixth() {
+	document.getElementById("sixth").onmouseenter = function () {
+		document.getElementById("sixth").style.color = "orange";
+	}
+}
+
+sixth();
+
+
+function six() {
+	document.getElementById("sixth").onmouseleave = function () {
+		document.getElementById("sixth").style.color = "black";
+	}
+}
+
+six();
+
+
+function seven() {
+	document.getElementById("seventh").onmouseenter = function () {
+		document.getElementById("seventh").style.color = "orange";
+	}
+}
+
+seven();
+
+
+function sev() {
+	document.getElementById("seventh").onmouseleave = function () {
+		document.getElementById("seventh").style.color = "black";
+	}
+}
+
+sev();
+
+
+
+function eight() {
+	document.getElementById("eight").onmouseenter = function () {
+		document.getElementById("eight").style.color = "orange";
+	}
+}
+
+eight();
+
+
+function eig() {
+	document.getElementById("eight").onmouseleave = function () {
+		document.getElementById("eight").style.color = "black";
+	}
+}
+
+eig();
+
+function nine() {
+	document.getElementById("ninee").onmouseenter = function () {
+		document.getElementById("ninee").style.color = "orange";
+	}
+}
+
+nine();
+
+
+function nin() {
+	document.getElementById("ninee").onmouseleave = function () {
+		document.getElementById("ninee").style.color = "black";
+	}
+}
+
+nin();
+
+
+function h() {
+	document.getElementById("tenth").onmouseenter = function () {
+		document.getElementById("tenth").style.color = "orange";
+	}
+}
+
+h();
+
+
+function ho() {
+	document.getElementById("tenth").onmouseleave = function () {
+		document.getElementById("tenth").style.color = "black";
+	}
+}
+
+ho();
+
+
+//a new line of touchstart code//
+//a comment to make me differenciate//
+
+
+function one() {
+	document.getElementById("first").ontouchstart = function () {
+		document.getElementById("first").style.color = "orange";
+	}
+}
+
+one();
+
+
+function two() {
+	document.getElementById("first").ontouchend = function () {
+		document.getElementById("first").style.color = "black";
+	}
+}
+
+two();
+
+function three() {
+	document.getElementById("second").ontouchstart = function () {
+		document.getElementById("second").style.color = "orange";
+	}
+}
+
+three();
+
+
+function four() {
+	document.getElementById("second").ontouchend = function () {
+		document.getElementById("second").style.color = "black";
+	}
+}
+
+four();
+
+
+function five() {
+	document.getElementById("third").ontouchstart = function () {
+		document.getElementById("third").style.color = "orange";
+	}
+}
+
+five();
+
+
+function eleven() {
+	document.getElementById("third").ontouchend = function () {
+		document.getElementById("third").style.color = "black";
+	}
+}
+
+eleven();
+
+
+
+
+
+function twelve() {
+	document.getElementById("fourth").ontouchstart = function () {
+		document.getElementById("fourth").style.color = "orange";
+	}
+}
+
+twelve();
+
+
+function thirteen() {
+	document.getElementById("fourth").ontouchend = function () {
+		document.getElementById("fourth").style.color = "black";
+	}
+}
+
+thirteen();
+
+function fourteen() {
+	document.getElementById("fifth").ontouchstart = function () {
+		document.getElementById("fifth").style.color = "orange";
+	}
+}
+
+fourteen();
+
+
+function fifteen() {
+	document.getElementById("fifth").ontouchend = function () {
+		document.getElementById("fifth").style.color = "black";
+	}
+}
+
+fifteen();
+
+
+function sixteen() {
+	document.getElementById("sixth").ontouchstart = function () {
+		document.getElementById("sixth").style.color = "orange";
+	}
+}
+
+sixteen();
+
+
+function seventeen() {
+	document.getElementById("sixth").ontouchend = function () {
+		document.getElementById("sixth").style.color = "black";
+	}
+}
+
+seventeen();
+
+
+function eighteen() {
+	document.getElementById("seventh").ontouchstart = function () {
+		document.getElementById("seventh").style.color = "orange";
+	}
+}
+
+eighteen();
+
+
+function nineteen() {
+	document.getElementById("seventh").ontouchend = function () {
+		document.getElementById("seventh").style.color = "black";
+	}
+}
+
+nineteen();
+
+
+
+function twenty() {
+	document.getElementById("eight").ontouchstart = function () {
+		document.getElementById("eight").style.color = "orange";
+	}
+}
+
+twenty();
+
+
+function ab() {
+	document.getElementById("eight").ontouchend = function () {
+		document.getElementById("eight").style.color = "black";
+	}
+}
+
+ab();
+
+function abc() {
+	document.getElementById("ninee").ontouchstart = function () {
+		document.getElementById("ninee").style.color = "orange";
+	}
+}
+
+abc();
+
+
+function abcd() {
+	document.getElementById("ninee").ontouchend = function () {
+		document.getElementById("ninee").style.color = "black";
+	}
+}
+
+abcd();
+
+
+function efg() {
+	document.getElementById("tenth").ontouchstart = function () {
+		document.getElementById("tenth").style.color = "orange";
+	}
+}
+
+efg();
+
+
+function hij() {
+	document.getElementById("tenth").ontouchend = function () {
+		document.getElementById("tenth").style.color = "black";
+	}
+}
+
+hij();
+
+function func() {
+	document.getElementById("shot").onclick = function () {
+		var a = confirm("Do you want to save this image?");
+
+		if (a == true) {
+			alert("saving image......");
+			
+		} else{
+			return false;
+		}
+
+
+	}
+}
+func();
+
+
+
+
+
+function my() {
+	
+	document.getElementById("pic").src = "tangerine-life-728x90-1.jpg";
+
+
+}
+setTimeout(my,2000);
+
+function scroll() {
+	document.getElementById("scroll").onclick = function () {
+		document.getElementById("frmContact").scrollIntoView();
+	}
+}
+
+scroll();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
